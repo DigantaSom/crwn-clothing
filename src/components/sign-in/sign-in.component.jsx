4ea1535d072568b9567620af86e2ea2935f2e234
Diganta Som <itsdsom111@gmail.com>
2021-03-23 20:15:09 +0530

    Added Sign in component UI

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,60 @@
+import { useState } from 'react';
+import CustomButton from '../custom-button/custom-button.component';
+import FormInput from '../form-input/form-input.component';
+import './sign-in.styles.scss';
+
+const SignIn = () => {
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+
+  const { email, password } = formData;
+
+  const handleChange = e => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value,
+    });
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    // TODO: handle sign in with firebase
+
+    setFormData({
+      ...formData,
+      [e.target.name]: '',
+    });
+  };
+
+  return (
+    <div className='sign-in'>
+      <h2 className='title'>I already have an account</h2>
+      <span>Sign in with your email and password</span>
+
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          type='email'
+          name='email'
+          value={email}
+          handleChange={handleChange}
+          label='email'
+          required
+        />
+        <FormInput
+          type='password'
+          name='password'
+          value={password}
+          handleChange={handleChange}
+          label='password'
+          required
+        />
+        <CustomButton type='submit'>Sign In</CustomButton>
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
